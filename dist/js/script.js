8c36fefb03807a0322c56a8da57be419330488ce
rikarani <phasyaananta1502@gmail.com>
2022-10-19 01:02:02 +0700

    ubah function, bikin event baru

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,7 @@
 // * Import Function
 import { checkTheme, keepTheme } from "../../src/js/theme/theme.checker.js";
 import { darkSwitch, lightSwitch } from "../../src/js/theme/theme.config.js";
-import { nilaiTriliun, getTriliun } from "../../src/js/utils/getTriliun.js";
-import { nilaiMiliar, getMiliar } from "../../src/js/utils/getMiliar.js";
-import { nilaiJuta, getJuta } from "../../src/js/utils/getJuta.js";
-import { nilaiRibu, getRibu } from "../../src/js/utils/getRibu.js";
+import { parser } from "../../src/js/app/parser.js";
 
 // * Cek Tema dulu
 checkTheme();
@@ -25,60 +22,73 @@ toggle.addEventListener("click", function () {
   }
 });
 
-const inputTerbilang = document.getElementById("erpe");
+const field = document.getElementById("erpe");
 
-inputTerbilang.addEventListener("input", function () {
-  // span buat tampilkan hasil terbilang
+// * trigger event pas ada yang inputkan angka
+field.addEventListener("input", function () {
   const spanTerbilang = document.getElementById("terbilang");
 
-  // Array buat nyimpan hasil convert
-  const hasil = [];
+  // * kalo inputan nya kosong (misal abis di-delete, di-cut) atau 0
+  if (field.value == "" || field.value == "0") {
+    const tombolSalin = document.getElementById("copyText");
 
-  // * get nilai Triliun
-  let triliun = Math.floor(getTriliun(inputTerbilang.value));
-  let sisaAngka = inputTerbilang.value % nilaiTriliun;
+    // * kosongin hasil terbilang nya
+    spanTerbilang.textContent = "";
 
-  // * kalo ada, tambahin ke array
-  if (triliun > 0) {
-    hasil.push(triliun, "Triliun");
+    // * matiin tombol nya
+    tombolSalin.disabled = true;
   }
+  // * kalo inputan nya melebihi batas
+  else if (field.value >= "9999999999999999") {
+    const tombolSalin = document.getElementById("copyText");
+    const warning = document.querySelector(".warning");
 
-  // * get nilai Miliar
-  let miliar = Math.floor(getMiliar(sisaAngka));
-  sisaAngka %= nilaiMiliar; // sisaAngka = sisaAngka % nilaiMiliar
+    // * kosongin hasil terbilang nya
+    spanTerbilang.textContent = "";
 
-  // * kalo ada, tambahin ke array
-  if (miliar > 0) {
-    hasil.push(miliar, "Miliar");
+    // * munculin pesan error
+    warning.classList.remove("hidden");
+
+    // * matiin tombol nya
+    tombolSalin.disabled = true;
   }
+  // * kalau normal yaa
+  else {
+    const tombolSalin = document.getElementById("copyText");
+    const warning = document.querySelector(".warning");
 
-  // * get nilai Juta
-  let juta = Math.floor(getJuta(sisaAngka));
-  sisaAngka %= nilaiJuta; // sisaAngka = sisaAngka % nilaiJuta
+    // * ubah tulisan terbilang sesuai angka yang di-inputkan
+    spanTerbilang.textContent = parser(field.value);
 
-  // * kalo ada, tambahin ke array
-  if (juta > 0) {
-    hasil.push(juta, "Juta");
+    // * sembunyikan pesan error nya
+    warning.classList.add("hidden");
+
+    // * nyalain tombol nya
+    tombolSalin.disabled = false;
   }
+});
 
-  // * get nilai Ribu
-  let ribu = Math.floor(getRibu(sisaAngka));
-  sisaAngka %= nilaiRibu; // sisaAngka = sisaAngka % nilaiRibu
+const tombolSalin = document.getElementById("copyText");
 
-  // * kalo ada, tambahin ke array
-  if (ribu > 0) {
-    hasil.push(ribu, "Ribu");
-  }
+tombolSalin.addEventListener("click", function () {
+  const tooltip = document.querySelector(".tooltip-text");
+  const spanTerbilang = document.getElementById("terbilang");
 
-  // * get nilai Satu
-  let satu = Math.floor(sisaAngka / 1);
-  sisaAngka %= 1; // sisaAngka = sisaAngka % 1
+  // * bikin objek stabilo (buat highlight tulisan nya nanti)
+  const selection = window.getSelection();
+  const range = document.createRange();
+  range.selectNode(spanTerbilang);
+  selection.removeAllRanges();
+  selection.addRange(range);
 
-  // * kalo ada, tambahin ke array
-  if (satu > 0) {
-    hasil.push(satu, "Rupiah");
-  }
+  // * copy ke clipboard
+  navigator.clipboard.writeText(spanTerbilang.textContent);
 
-  console.log(hasil);
-  console.log(sisaAngka);
+  // * munculin tooltip nya pas tombol di-klik
+  tooltip.classList.toggle("opacity-0");
+
+  // * sembunyiin tooltip nya setelah 0.8s
+  setTimeout(() => {
+    tooltip.classList.toggle("opacity-0");
+  }, 800);
 });
